c63d9a71c38627f8f4c36a20319d95d8943bcab2
Lucas Francisconi Flores <lucas.flores@valtech.com>
2023-06-22 12:30:10 -0300

    AEM Newsletter component 2.0

--- core/src/main/java/com/aem/training/core/servlets/SubscribeNewsletterServlet.java
+++ core/src/main/java/com/aem/training/core/servlets/SubscribeNewsletterServlet.java
@@ -1,11 +1,15 @@
 package com.aem.training.core.servlets;
 
 import com.day.cq.commons.jcr.JcrUtil;
+import org.apache.commons.io.IOUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
@@ -18,16 +22,15 @@ import javax.servlet.Servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import static org.apache.sling.api.servlets.HttpConstants.METHOD_GET;
 import static org.apache.sling.api.servlets.ServletResolverConstants.*;
 
 @Component (
         service = Servlet.class,
         immediate = true,
         property = {
-            Constants.SERVICE_DESCRIPTION + "=Servlet used to store subscribers' data into JCR.",
-            SLING_SERVLET_PATHS + "=/bin/newsletter/subscribe",
-            SLING_SERVLET_METHODS + "=POST"
+                Constants.SERVICE_DESCRIPTION + "=Servlet used to store subscribers' data into JCR node.",
+                SLING_SERVLET_METHODS + "=POST",
+                SLING_SERVLET_PATHS + "=/bin/newsletter/subscribe"
         }
 )
 public class SubscribeNewsletterServlet extends SlingAllMethodsServlet {
@@ -38,16 +41,16 @@ public class SubscribeNewsletterServlet extends SlingAllMethodsServlet {
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) {
 
-        ResourceResolver resourceResolver = request.getResourceResolver();
-        Session session = resourceResolver.adaptTo(Session.class);
-
-        String fName = request.getParameter("name");
-        String lName = request.getParameter("surname");
-        String email = request.getParameter("email");
-        String monthOfBirth = request.getParameter("anniversary_month");
-        String authorize = request.getParameter("authorize");
-
         try {
+            ResourceResolver resourceResolver = request.getResourceResolver();
+            Session session = resourceResolver.adaptTo(Session.class);
+
+            String fName = request.getParameter("name");
+            String lName = request.getParameter("surname");
+            String email = request.getParameter("email");
+            String monthOfBirth = request.getParameter("anniversary_month");
+            String authorize = request.getParameter("authorize");
+
             PrintWriter out = response.getWriter();
 
             Node subscribers = JcrUtil.createPath(CREATE_PATH + "/items", "nt:unstructured", session);
--- ui.frontend/src/components/Subscribe/Subscribe.js
+++ ui.frontend/src/components/Subscribe/Subscribe.js
@@ -2,87 +2,100 @@ import React, { Component } from 'react';
 import { MapTo } from '@adobe/aem-react-editable-components';
 require('./Subscribe.css');
 
-const SubscribeConfig = {
-    emptyLabel: 'Subscribe Form',
-
-    isEmpty: function(props) {
-        return !props || !props.title || !props.label || !props.authorization;
+const Subscribe = ({ title, label, authorization }) => {
+    if (!title || !label || !authorization) {
+        return null;
     }
+
+    const handleFormSubmit = async (event) => {
+    event.preventDefault();
+        try {
+            const form = event.target;
+            const formData = new FormData(form);
+            await submitForm(formData);
+        } catch (error) {}
+    };
+
+    const submitForm = async (formData) => {
+    console.log('>>>>>>>>>>Form submitted<<<<<<<<<');
+        try {
+            await fetch('/bin/newsletter/subscribe', {
+                method: 'POST',
+                body: formData,
+        });
+        } catch (error) {}
+    };
+
+    return (
+        <div>
+            <Title title={title} />
+            <Form onSubmit={handleFormSubmit} label={label} authorization={authorization} />
+        </div>
+    );
 };
 
-export default class Subscribe extends Component {
-
-    get title() {
-        const title = this.props.title;
-        const doidera = window.location.href;
-        console.log("Teste de URL", doidera)
-        return (
-            <h3 class="cmp-title">{title}</h3>
-        )
-    }
-
-    get form() {
-        const authorizationMessage = this.props.authorization;
-        const btnLabel = this.props.label;
-
-        return (
-            <div class="cmp-form">
-                <form class="cmp-form__" method="POST" action="newsletter.subscribers.json">
-                    <div class="form-group">
-                        <label for="form_name">First name *</label>
-                        <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required."/>
-                    </div>
-                    <div class="form-group">
-                        <label for="form_lastname">Last name *</label>
-                        <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required."/>
-                    </div>
-                    <div class="form-group">
-                        <label for="form_email">Email *</label>
-                        <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required."/>
-                    </div>
-                    <div class="form-group">
-                        <label for="form_anniversary_month">Please specify your month of birthday *</label>
-                        <select id="form_anniversary_month" name="anniversary_month" class="form-control-select" required="required" data-error="Please specify your need.">
-                            <option value="none" selected disabled>--Select--</option>
-                            <option value="january">January</option>
-                            <option value="february">February</option>
-                            <option value="march">March</option>
-                            <option value="april">April</option>
-                            <option value="may">May</option>
-                            <option value="june">June</option>
-                            <option value="july">July</option>
-                            <option value="August">August</option>
-                            <option value="september">September</option>
-                            <option value="october">October</option>
-                            <option value="november">November</option>
-                            <option value="december">December</option>
-                        </select>
-                    </div>
-                    <div class="cmp-checkbox">
-                        <input type="checkbox" id="checkbox" name="authorize" required="required" data-error="To check the box is required."/>
-                        <p dangerouslySetInnerHTML={{ __html: authorizationMessage }}></p>
-                    </div>
-                    <div>
-                        <input type="submit" value={btnLabel} class="btn"/>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-
-    render() {
-        if(SubscribeConfig.isEmpty(this.props)) {
-            return null;
-        }
-
-        return (
-            <div>
-                <div>{this.title}</div>
-                <div>{this.form}</div>
-            </div>
-        )
-    }
-
+const Title = ({ title }) => {
+    return <h3 className="cmp-title">{title}</h3>;
 };
 
-MapTo('aem-training-03/components/subscribe-form')(Subscribe, SubscribeConfig);
+const Form = ({ onSubmit, label, authorization, handleFormSubmit }) => {
+    return (
+        <div className="cmp-form">
+            <form className="cmp-form__" onSubmit={onSubmit} action={handleFormSubmit}>
+                <FormGroup label="First name *" id="form_name" name="name" placeholder="Please enter your firstname *" required />
+                <FormGroup label="Last name *" id="form_lastname" name="surname" placeholder="Please enter your lastname *" required />
+                <FormGroup label="Email *" id="form_email" name="email" placeholder="Please enter your email *" required type="email" />
+                <FormGroupSelect
+                    label="Please specify your month of birthday *"
+                    id="form_anniversary_month"
+                    name="anniversary_month"
+                    required>
+                    <option value="none" selected disabled>--Select--</option>
+                    <option value="january">January</option>
+                    <option value="february">February</option>
+                    <option value="march">March</option>
+                    <option value="april">April</option>
+                    <option value="may">May</option>
+                    <option value="june">June</option>
+                    <option value="july">July</option>
+                    <option value="August">August</option>
+                    <option value="september">September</option>
+                    <option value="october">October</option>
+                    <option value="november">November</option>
+                    <option value="december">December</option>
+                </FormGroupSelect>
+                <div className="cmp-checkbox">
+                    <input type="checkbox" id="checkbox" name="authorize" required />
+                    <p dangerouslySetInnerHTML={{ __html: authorization }} />
+                </div>
+                <div>
+                    <input type="submit" value={label} className="btn" />
+                </div>
+            </form>
+        </div>
+    );
+};
+
+const FormGroup = ({ label, id, name, placeholder, required, type }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type={type || 'text'} name={name} className="form-control" placeholder={placeholder} required={required} />
+    </div>
+  );
+};
+
+const FormGroupSelect = ({ label, id, name, required, children }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{label}</label>
+      <select id={id} name={name} className="form-control-select" required={required}>
+        {children}
+      </select>
+    </div>
+  );
+};
+
+export default Subscribe;
+
+MapTo('aem-training-03/components/subscribe-form')(Subscribe, Form, FormGroup, FormGroupSelect);
